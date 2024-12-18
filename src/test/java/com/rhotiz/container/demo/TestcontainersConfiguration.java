409e575b380277441076b739ca31cd96e7ce9667
Ahmad Hoghooghi <hoghooghi@inside.sahab.ir>
2024-12-18 17:05:03 +0330

    Fix execute kafka problem:
    change
    kafka-native:3.9.0 to
    confluentinc/cp-kafka:7.8.0
    
    change  KafkaContainer (deprecated) to
    ConfluentKafkaContainer

--- src/test/java/com/rhotiz/container/demo/TestcontainersConfiguration.java
+++ src/test/java/com/rhotiz/container/demo/TestcontainersConfiguration.java
@@ -3,7 +3,7 @@ package com.rhotiz.container.demo;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -11,8 +11,9 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.9.0"));
+	ConfluentKafkaContainer kafkaContainer() {
+		DockerImageName imageName = DockerImageName.parse("confluentinc/cp-kafka:7.8.0");
+		return new ConfluentKafkaContainer(imageName);
 	}
 
 }
