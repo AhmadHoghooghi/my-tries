6ddb1afbc43a1f1ec7dd6bf8a4443efc675fc114
Ahmad Hoghooghi <ahmad.hoghooghi@gmail.com>
2025-01-30 15:29:58 +0330

    Add test structure to spark-app

---
+++ spark-app/src/test/java/com/rhotiz/spark/SparkAppIT.java
@@ -0,0 +1,11 @@
+package com.rhotiz.spark;
+
+import org.junit.jupiter.api.Test;
+
+public class SparkAppIT {
+
+    @Test
+    void runIntegrationTest() {
+        System.out.println("Running Integration Test");
+    }
+}
---
+++ spark-app/src/test/java/com/rhotiz/spark/SparkAppUnitTest.java
@@ -0,0 +1,12 @@
+package com.rhotiz.spark;
+
+import org.junit.jupiter.api.Test;
+
+public class SparkAppUnitTest {
+
+    @Test
+    void sparkrAppUnitTest() {
+        System.out.println("Unit test is executed");
+    }
+
+}
